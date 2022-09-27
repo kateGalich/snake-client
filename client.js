6fb8c707e62397da5dca0ab74d379824d1dc6ef6
Kate Galich <kate.galich@outlook.com>
2022-09-27 06:18:18 -0400

    Constants Module

--- client.js
+++ client.js
@@ -1,9 +1,12 @@
+const { IP, PORT } = require('./constants');
+
+
 const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541// PORT number here,
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
